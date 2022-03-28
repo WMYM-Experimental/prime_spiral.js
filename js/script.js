459cdc311f20f4cc47b37db6e02a1465fe650319
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2022-03-28 13:09:31 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.hight = 500
+canvas.width = 500
+
+class Dot{
+    constructor(x,y, radius, color){
+        
+    }
+}
